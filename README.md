7d54e4b72ce0a836aeaa72466ee4d4c965b3a5ef
wuzhifeng <wzf@yn-ce.com>
2019-06-17 11:27:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Java使用Redis实现发布与订阅
+
+
+### 1.安装redis
+````
+docker pull redis
+docker run -d --name redis  -p 6379:6379 --name redis redis
+````
+
+### 2.编译
+````
+cd java-redis-pub-sub
+mvn install
+````
+
+### 3.启动订阅程序
+````
+mvn exec:java -Dexec.mainClass="com.frontng.demo.redispubsub.Sub"
+````
+
+### 4.启动发布程序
+````
+mvn exec:java -Dexec.mainClass="com.frontng.demo.redispubsub.Pub"
+````
---
+++ src/main/java/com/frontng/demo/redispubsub/RedisUtils.java
@@ -0,0 +1,22 @@
+package com.frontng.demo.redispubsub;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * redis工具类
+ * @author Front Ng
+ * @date 2019-06-14 11:46
+ **/
+public class RedisUtils {
+
+    private final static JedisPoolConfig POOL_CONFIG = new JedisPoolConfig();
+
+    private final static JedisPool JEDIS_POOL = new JedisPool(POOL_CONFIG, "127.0.0.1", 6379, 0);
+
+    public static Jedis getJedis() {
+        return JEDIS_POOL.getResource();
+
+    }
+}
